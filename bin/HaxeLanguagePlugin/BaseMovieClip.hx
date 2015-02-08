cf5df9a51b07daeb95ab29404f20c230019b9c44
Yaroslav Sivakov <yar3333@gmail.com>
2015-02-08 04:46:51 +0300

    * remove unnecessary cast

--- bin/HaxeLanguagePlugin/BaseMovieClip.hx
+++ bin/HaxeLanguagePlugin/BaseMovieClip.hx
@@ -2,5 +2,5 @@ package {pack};
 
 class {klass} extends {base}
 {
-	public function new() super(cast nanofl.Player.library.getItem("{namePath}"));
+	public function new() super(nanofl.Player.library.getItem("{namePath}"));
 }
