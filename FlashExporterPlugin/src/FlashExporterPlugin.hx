6710595763f6f465e37630453cf77e36bd7f8993
Yaroslav Sivakov <yar3333@gmail.com>
2015-07-04 19:09:51 +0300

    * rename flash plugins

--- FlashExporterPlugin/src/FlashExporterPlugin.hx
+++ FlashExporterPlugin/src/FlashExporterPlugin.hx
@@ -6,20 +6,20 @@ import nanofl.engine.Plugins;
 import nanofl.ide.plugins.IExporterPlugin;
 using StringTools;
 
-class FlashXflExporterPlugin implements IExporterPlugin
+class FlashExporterPlugin implements IExporterPlugin
 {
 	static var embeddedIcon = "iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAYAAAAfSC3RAAAACXBIWXMAAAsSAAALEgHS3X78AAACNUlEQVQoz21Qz0uUURQ997033zhjZvhjapps1RAkqYt0U1O4MAiLwUWEhEEQtKl2LdoEIkR/gFsX1S4iZgjbJiFEizSwTCgoYhgtSUTnm5nP+d47LWZGgzxwuffCORzOEZKYm5szw8PDvel0+nilUqEAAhIAAKUoIiYMwz/FYnEhl8v52WxWQBL5fB4jIyOTJFmtVnf4D6y1liQLhcLP0dHR6wAi+XweBgBmZ2fR0tIiALDt+64WBI5fv8FqjSB1FFok7O7u7pmenn5sjGE2m81hdXVVAODGxMQUSW5tbOxUnzzjzoP7LN+5xw3f59bmJq21oXOO6+vrhVQqdd6ICACA1tb38xeQd2/haiHcwQ5EvAjCIIDv+7pSqYSJRCKltT5k2ChB2tqAtd9QS4tgZxfkyhi8k2lY60BjEIYhrLUKAEjWDwBgtQosfEC4tAz2n4EaGoRKJqGVQiQSQTQaRSwWa9KVYq2mAcB8/uKwsgxvsI/hzAyCh5OAtVBKQWsNrTWMMU0hjBMJz7W2tt+OxQf4aQnOiWJnArrnWJ3R6EBEoJTaEyYXP2beDAxdCFKHLweVCvXYNR29OgZXKoEk6NyucLdIEsafevQyTCQ6zHZJSV8/vEsXQRFgL8++jspVgy7v+w8XP5Isyfg42HbA2XLZ0dVB0pHNr2EPULW/fnX2/a2bQ3d/rT2Nne4Fg8B68biKeJ7y9pmGO4Uk5ufnJZPJnEqn0yfK5bKVZpj/QRFhsVhc+QuQDi4zdLU6egAAAABJRU5ErkJggg==";
 	
-	static function main() Plugins.registerExporter(new FlashXflExporterPlugin());
+	static function main() Plugins.registerExporter(new FlashExporterPlugin());
 	
 	public function new() { }
 	
-	public var name = "FlashXflExporter";
+	public var name = "FlashExporter";
 	
-	public var menuItemName = "Adobe Flash Uncompressed Document (*.xfl)";
+	public var menuItemName = "Adobe Flash Document (*.fla;*.xfl)";
 	public var menuItemIcon = "url(data:image/png;base64," + embeddedIcon.replace("\r", "").replace("\n", "") + ")";
-	public var fileFilterDescription = "Adobe Flash Document (*.xfl)";
-	public var fileFilterExtensions = [ "xfl" ];
+	public var fileFilterDescription = "Adobe Flash Document (*.fla;*.xfl)";
+	public var fileFilterExtensions = [ "fla", "xfl" ];
 	
 	public function exportDocument(fileApi:FileApi, srcFilePath:String, destFilePath:String, documentProperties:DocumentProperties, library:Library) : Bool
 	{
--- FlashImporterPlugin/src/FlashImporterPlugin.hx
+++ FlashImporterPlugin/src/FlashImporterPlugin.hx
@@ -7,7 +7,7 @@ import nanofl.engine.Plugins;
 import nanofl.ide.plugins.IImporterPlugin;
 using StringTools;
 
-class FlashXflImporterPlugin implements IImporterPlugin
+class FlashImporterPlugin implements IImporterPlugin
 {
 	static var embeddedIcon = "iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAYAAAAfSC3RAAAACXBIWXMAAAsSAAALEgHS3X78AAACNUlEQVQoz21Qz0uUURQ997033zhjZvhjapps1RAkqYt0U1O4MAiLwUWEhEEQtKl2LdoEIkR/gFsX1S4iZgjbJiFEizSwTCgoYhgtSUTnm5nP+d47LWZGgzxwuffCORzOEZKYm5szw8PDvel0+nilUqEAAhIAAKUoIiYMwz/FYnEhl8v52WxWQBL5fB4jIyOTJFmtVnf4D6y1liQLhcLP0dHR6wAi+XweBgBmZ2fR0tIiALDt+64WBI5fv8FqjSB1FFok7O7u7pmenn5sjGE2m81hdXVVAODGxMQUSW5tbOxUnzzjzoP7LN+5xw3f59bmJq21oXOO6+vrhVQqdd6ICACA1tb38xeQd2/haiHcwQ5EvAjCIIDv+7pSqYSJRCKltT5k2ChB2tqAtd9QS4tgZxfkyhi8k2lY60BjEIYhrLUKAEjWDwBgtQosfEC4tAz2n4EaGoRKJqGVQiQSQTQaRSwWa9KVYq2mAcB8/uKwsgxvsI/hzAyCh5OAtVBKQWsNrTWMMU0hjBMJz7W2tt+OxQf4aQnOiWJnArrnWJ3R6EBEoJTaEyYXP2beDAxdCFKHLweVCvXYNR29OgZXKoEk6NyucLdIEsafevQyTCQ6zHZJSV8/vEsXQRFgL8++jspVgy7v+w8XP5Isyfg42HbA2XLZ0dVB0pHNr2EPULW/fnX2/a2bQ3d/rT2Nne4Fg8B68biKeJ7y9pmGO4Uk5ufnJZPJnEqn0yfK5bKVZpj/QRFhsVhc+QuQDi4zdLU6egAAAABJRU5ErkJggg==";
 	
@@ -17,16 +17,16 @@ class FlashXflImporterPlugin implements IImporterPlugin
 	static var IMPORT_MEDIA_SCRIPT_TEMPLATE = Macro.embedFile("../../tools/FlashMediaImporter/bin/FlashMediaImporter.jsfl");
 	#end
 	
-	static function main() Plugins.registerImporter(new FlashXflImporterPlugin());
+	static function main() Plugins.registerImporter(new FlashImporterPlugin());
 	
 	public function new() { }
 	
-	public var name = "FlashXflImporter";
+	public var name = "FlashImporter";
 	
-	public var menuItemName = "Adobe Flash Uncompressed Document (*.xfl)";
+	public var menuItemName = "Adobe Flash Document (*.fla;*.xfl)";
 	public var menuItemIcon = "url(data:image/png;base64," + embeddedIcon.replace("\r", "").replace("\n", "") + ")";
-	public var fileFilterDescription = "Adobe Flash Uncompressed Document (*.xfl)";
-	public var fileFilterExtensions = [ "xfl" ];
+	public var fileFilterDescription = "Adobe Flash Document (*.fla;*.xfl)";
+	public var fileFilterExtensions = [ "fla", "xfl" ];
 	
 	public function importDocument(fileApi:FileApi, srcFilePath:String, destFilePath:String, documentProperties:DocumentProperties, library:Library, fonts:Array<String>, callb:Bool->Void)
 	{
