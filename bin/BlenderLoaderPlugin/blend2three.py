21df858b607c767b3217b3bd55aa20713ad5d10a
Yaroslav Sivakov <yar3333@gmail.com>
2016-01-14 23:32:13 +0300

    * blend2three: option_face_materials=True, option_hierarchy=True

--- bin/BlenderLoaderPlugin/blend2three.py
+++ bin/BlenderLoaderPlugin/blend2three.py
@@ -14,4 +14,41 @@ for i in range(1, len(sys.argv)):
     if sys.argv[i] == "--":
         break
 
-bpy.ops.export.three(filepath=sys.argv[i+1])
+#filepath=""
+#check_existing=True
+#option_vertices=True
+#option_faces=True
+#option_normals=True
+#option_colors=False
+#option_mix_colors=False
+#option_uv_coords=True
+#option_materials=False
+#option_face_materials=False
+#option_maps=False
+#option_skinning=False
+#option_bones=False
+#option_extra_vgroups=""
+#option_apply_modifiers=True
+#option_index_type='Uint16Array'
+#option_scale=1
+#option_round_off=True
+#option_round_value=6
+#option_logging='disabled'
+#option_geometry_type='geometry'
+#option_export_scene=False
+#option_embed_animation=True
+#option_copy_textures=True
+#option_texture_folder=""
+#option_lights=False
+#option_cameras=False
+#option_hierarchy=False
+#option_animation_morph=False
+#option_blend_shape=False
+#option_animation_skeletal='off'
+#option_keyframes=False
+#option_frame_index_as_time=False
+#option_frame_step=1
+#option_indent=True
+#option_compression='None'
+#option_influences=2
+bpy.ops.export.three(filepath=sys.argv[i+1], option_face_materials=True, option_hierarchy=True)
