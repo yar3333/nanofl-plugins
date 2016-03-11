42a1d7b6a959f116551eab002607575a66a32a1a
Yaroslav Sivakov <yar3333@gmail.com>
2016-03-11 23:56:26 +0300

    * Player externs fix

--- generators/CreateJSGeneratorPlugin/support/languages/Haxe/files/gen/nanofl/Player.hx
+++ generators/CreateJSGeneratorPlugin/support/languages/Haxe/files/gen/nanofl/Player.hx
@@ -7,5 +7,5 @@ extern class Player
 	static var scene : nanofl.MovieClip;
 	static var spriteSheets : Dynamic<createjs.SpriteSheet>;
 	
-	static function init(canvas:js.html.CanvasElement, library:Library, ?framerate:Float, ?scaleMode:String, ?textureAtlasesData:Array<Dynamic<{ images:Array<String>, frames:Array<Array<Int>> }>>) : Void
+	static function init(canvas:js.html.CanvasElement, library:Library, ?framerate:Float, ?scaleMode:String, ?textureAtlasesData:Array<Dynamic<{ images:Array<String>, frames:Array<Array<Int>> }>>) : Void;
 }
